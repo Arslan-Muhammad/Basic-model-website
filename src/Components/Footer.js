18e397a467634df97d22853e4b889fdd051f4f09
Arslan <muhammad.arslan.82jb@gmail.com>
2023-09-26 20:59:42 +0500

    footer changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <section className="footer text-center"><strong>@Copyright<sup>R</sup> by mrzahidfaiz 2023</strong></section>
+    <section className="footer text-center"><strong>@Copyright<sup>R</sup> by Arslan 2023</strong></section>
   )
 }
